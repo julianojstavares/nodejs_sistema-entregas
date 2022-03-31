4ae102c1067d60c60d6e662c42710d4f8e71e34d
Juliano J. S. Tavares <julianojstavares@outlook.com>
2022-03-31 20:48:44 -0300

    Working on Auth deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import { CreateClientController } from './modules/clients/useCases/createClient/createClientController';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
-routes.post("/client/", createClientController.handle);
+routes.post("/client", createClientController.handle);
 
 const authenticateClientController = new AuthenticateClientController();
-routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/client/authenticate", authenticateClientController.handle);
+
+const createDeliverymanController = new CreateDeliverymanController();
+routes.post("/deliveryman", createDeliverymanController.handle);
+
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
 
 export { routes };
